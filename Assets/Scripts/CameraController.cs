a85acbc8ae555828fe82e23f10bace3c885e2276
a22i086 <a22i086tw@ous.jp>
2025-01-20 01:39:49 +0900

    feat：カメラ操作の変更

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,38 +1,47 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour
 {
     public float panSpeed = 20f; // カメラの移動速度
+    public Scrollbar scrollbar;
     private Vector3 initialPosition;
+
+    public float minHeight = -60f;
+    public float maxHeight = 20f;
 
     void Start()
     {
         // カメラの初期位置を保存
         initialPosition = transform.position;
+        // スクロールバーの値を変更する
+        scrollbar.onValueChanged.AddListener(OnScrollbarValueChanged);
     }
 
     void Update()
     {
         // 現在のカメラ位置を取得
         Vector3 pos = transform.position;
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.y -= scroll * panSpeed * 10f;
+        pos.y = Mathf.Clamp(pos.y, 1f, 50f);
 
         if (Input.GetMouseButton(1)) // 右クリックを押している間
         {
-            float move = panSpeed * 0.01f;
+            float rotationSpeed = 200.0f;
 
-            // Z方向にパン
-            if (Input.GetAxis("Mouse Y") < 0) // 前
-            {
-                pos.z += move;
-            }
-            if (Input.GetAxis("Mouse Y") > 0) // 後ろ
-            {
-                pos.z -= move;
-            }
+            //float h = rotationSpeed * Input.GetAxis("Mouse X") * Time.deltaTime;
+            float v = rotationSpeed * Input.GetAxis("Mouse Y") * Time.deltaTime;
+
+            transform.Rotate(v, 0, 0);
         }
+    }
 
-        // 高さを維持
-        pos.y = initialPosition.y;
-        transform.position = pos;
+    void OnScrollbarValueChanged(float value)
+    {
+        float newZ = Mathf.Lerp(minHeight, maxHeight, value);
+        Vector3 newPosition = initialPosition;
+        newPosition.z = newZ;
+        transform.position = newPosition;
     }
 }
